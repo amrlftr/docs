3c29dd3b75ec2248dbff4ce146fc0957a7c39b85
nic | nic.ethkl.eth <nicholaschn@protonmail.com>
2025-04-08 10:25:39 +0800

    Documentation edits made through Mintlify web editor

--- custom.js
+++ custom.js
@@ -1,1 +1,12 @@
-console.log(window.location.href);
+// Function to get query parameters by name
+function getQueryParam(name) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
+
+// Retrieve the 'chainid' parameter from the URL
+const chainId = getQueryParam('chainid');
+
+// Make chainId globally accessible
+window.chainId = chainId;
+console.log(chainId);
