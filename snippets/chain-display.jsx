0a2d8cc308a8aeb182cacdda8404ecc9e3c15f9f
nic | nic.ethkl.eth <nicholaschn@protonmail.com>
2025-07-04 01:03:18 +0800

    Add React guide and dynamic chain display

---
+++ snippets/chain-display.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+export const ChainDisplay = () => {
+  const [chain, setChain] = useState(1);
+
+  useEffect(() => {
+    const stored = localStorage.getItem('chain');
+    if (stored) {
+      setChain(Number(stored));
+    }
+  }, []);
+
+  return <span>{chain}</span>;
+};
